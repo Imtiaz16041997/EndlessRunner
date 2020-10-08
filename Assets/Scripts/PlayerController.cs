710b7c5b7601c97090cdba4ebeb95f85e05e9845
imtiaz eemel <imtiazeemel42@gmail.com>
2020-10-08 16:57:30 +0600

    SecondCommit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool jump = false;
+    public bool slide = false;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {     
+         transform.Translate(0, 0,0.01f);  // this is for Running Code 
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            slide = true;
+        }
+        else
+        {
+            slide = false;
+        }
+
+
+
+        if (jump == true)  // This is for Jumping
+        {
+            anim.SetBool("isJump", jump);
+            transform.Translate(0, 0.02f, 0.01f);
+
+        }
+        else if (jump == false)
+        {
+            anim.SetBool("isJump", jump);
+            //transform.Translate(0, 0, 0.01f);
+
+        }
+
+
+        if (slide == true)  // This is for Sliding part 
+        {
+            anim.SetBool("isSlide", slide);
+            transform.Translate(0, 0, 0.01f);
+
+        }
+        else if (slide == false)
+        {
+            anim.SetBool("isSlide", slide);
+            //transform.Translate(0, 0, 0.01f);
+
+        }
+
+    }
+}
